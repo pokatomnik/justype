5024ef4e63b13dd05c8169e8ea96939ec73b7166
Danilian Akhmedzianov <hugefast@gmail.com>
2021-09-11 00:47:26 +0300

    implement textarea autofocus

---
+++ ui/components/editor/Textarea.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import styles from './Textarea.module.css';
+
+interface ITextareaProps {
+  text: string;
+  onChange: (text: string) => void;
+}
+
+export function Textarea(props: ITextareaProps) {
+  const textareaRef = React.useRef<HTMLTextAreaElement>(null);
+
+  const textareaHandleChange = React.useCallback(
+    (evt: React.ChangeEvent<HTMLTextAreaElement>) => {
+      props.onChange(evt.currentTarget.value);
+    },
+    [props.onChange]
+  );
+
+  React.useLayoutEffect(() => {
+    textareaRef.current?.focus();
+  }, []);
+
+  return (
+    <textarea
+      ref={textareaRef}
+      placeholder="Type here... (Double click to toggle Markdown View mode)"
+      className={styles.textarea}
+      value={props.text}
+      onChange={textareaHandleChange}
+    />
+  );
+}
